97113d340990e8b92ffd7088ab67ee9278156e5c
castroguillermo <guillermocastro0806@gmail.com>
2026-07-03 06:01:15 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
 //  3) Se actualice sola cuando subas una versión nueva
 // ============================================================
 
-const VERSION = "v2";
+const VERSION = "v3";
 const CACHE = `mesa-cambio-${VERSION}`;
 
 const APP_SHELL = [
